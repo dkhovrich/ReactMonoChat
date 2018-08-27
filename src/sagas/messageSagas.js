fbf7e84baeff9ef72c3c9f381212f79f23c01a5c
Dmitriy Khovrich <dkhovrich@lohika.com>
2018-08-27 17:41:43 +0300

    saga tests

--- src/sagas/messageSagas.js
+++ src/sagas/messageSagas.js
@@ -3,7 +3,7 @@ import { put, call } from 'redux-saga/effects'
 
 import { ADD_MESSAGE_REQUEST, ADD_MESSAGE_SUCCESS } from '../actions/messageActions';
 
-function* addMessage(action) {
+export function* addMessage(action) {
   yield call(delay, 1000);
   yield put({ type: ADD_MESSAGE_SUCCESS, payload: action.payload });
 }
